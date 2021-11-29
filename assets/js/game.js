566e83be9d41231d2876f2f00ad78c9270172eb8
Albert ML <albert.ml@protonmail.ch>
2021-11-29 02:23:24 -0500

    Initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,47 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What's your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        enemyHealth -= playerAttack;
+        console.log(playerName + " attacked " + enemyName + " for " + playerAttack + " damage!");
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        } else {
+            window.alert(enemyName + " now has " + enemyHealth + " HP!");
+        }
+
+        playerHealth -= enemyAttack;
+        console.log(enemyName + " attacked " + playerName + " for " + enemyAttack + " damage!");
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        } else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    }
+
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure?");
+        if (confirmSkip) {
+            playerMoney = playerMoney - 2;
+        } else {
+            fight();
+        }
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
+        fight();
+    }
+};
+
+fight();
